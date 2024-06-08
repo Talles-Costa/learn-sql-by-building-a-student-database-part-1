2feddca3bca9be7812a92c65cc1bb57b37ad8d8e
Talles Costa <tallesfpn@gmail.com>
2024-06-08 14:56:14 +0000

    Save progress: 840.1

--- insert_data.sh
+++ insert_data.sh
@@ -22,11 +22,12 @@ do
       fi
 
       # get new major_id
-      MAJOR_ID=$($PSQL "SELECT major_id FROM majors WHERE major='$MAJOR'"
+      MAJOR_ID=$($PSQL "SELECT major_id FROM majors WHERE major='$MAJOR'")
 
     fi
 
     # get course_id
+    COURSE_ID=$($PSQL "SELECT course_id FROM courses WHERE course='$COURSE'")
 
     # if not found
 
